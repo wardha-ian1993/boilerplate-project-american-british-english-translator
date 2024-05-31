3c1d121730c2ae58c653de37820b63866c8a7474
wardha-ian1993 <favianwardhana1993@gmail.com>
2024-05-31 21:37:22 +0800

    feat: add api handler

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,28 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const { text, locale } = req.body;
+      const isLocaleValid = [
+        'american-to-british', 
+        'british-to-american'
+      ].includes(locale);
+
+      if (!text) return res.json({ error: 'No text to translate' });
+      if (!locale) return res.json({ error: 'Required field(s) missing' });
+      if (!isLocaleValid) return res.json({ error: 'Invalid value for locale field' });
       
+      let result;
+      switch (true) {
+        case (locale === 'american-to-british'):
+          result = translator.americanToBritish(text);
+          break;
+        case (locale === 'british-to-american'):
+          result = translator.britishToAmerican(text);
+          break;
+        default:
+          break;
+      }
+      
+      return res.json(result);
     });
 };
